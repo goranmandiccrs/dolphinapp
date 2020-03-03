a2be4f9a179a9dc400a7ec27a409240d54bb0bab
goranmandiccrs <goran.mandic@crowdedroomstudio.com>
2020-03-03 20:56:38 +0100

    added image picker

--- screens/ServiceReport.tsx
+++ screens/ServiceReport.tsx
@@ -1,9 +1,14 @@
 import { observer } from "mobx-react";
-import { Button, Text, TextInput, View } from "react-native";
+import { Button, Image, Text, TextInput, View } from "react-native";
 import DateTimePicker from "@react-native-community/datetimepicker";
 import React, { useState } from "react";
 import { useMst } from "../state/RootModel";
 import RNDateTimePicker from "@react-native-community/datetimepicker";
+//@ts-ignore
+import Upload from "../assets/camera.svg";
+//@ts-ignore
+import Picture from "../assets/picture.svg";
+import PhotoUpload from "react-native-photo-upload";
 
 export const ServiceReport = observer(({ navigation }) => {
   const {
@@ -24,44 +29,49 @@ export const ServiceReport = observer(({ navigation }) => {
     },
   } = useMst();
 
+  const styles = {
+    upload: {},
+  };
   return (
-    <View>
-      <Text>Pool Name / Address</Text>
-      <TextInput onChangeText={(text) => setName(text)} />
-      <Text>Date</Text>
-      <Text onPress={onFocusDatePicker}>{reportDate.toDateString()}</Text>
-      <Text>Time</Text>
-      <Text onPress={onFocusTimePicker}>{time}</Text>
-
-      {/*<View>
-        <Button onPress={showDatePicker} title="Show date picker!" />
-      </View>
+    <>
       <View>
-        <Button onPress={showTimePicker} title="Show time picker!" />
-      </View>*/}
-      {isDatePickerVisible && (
-        <DateTimePicker
-          testID="dateTimePicker"
-          timeZoneOffsetInMinutes={0}
-          value={reportDate}
-          //@ts-ignore
-          mode={"date"}
-          display="default"
-          onChange={onChange}
-        />
-      )}
-      {isTimePickerVisible && (
-        <DateTimePicker
-          testID="dateTimePicker"
-          timeZoneOffsetInMinutes={0}
-          value={reportTime}
-          //@ts-ignore
-          mode={"time"}
-          is24Hour={true}
-          display="default"
-          onChange={onChange}
-        />
-      )}
-    </View>
+        <Text>Pool Name / Address</Text>
+        <TextInput onChangeText={(text) => setName(text)} />
+        <Text>Date</Text>
+        <Text onPress={onFocusDatePicker}>{reportDate.toDateString()}</Text>
+        <Text>Time</Text>
+        <Text onPress={onFocusTimePicker}>{time}</Text>
+
+        {isDatePickerVisible && (
+          <DateTimePicker
+            testID="dateTimePicker"
+            timeZoneOffsetInMinutes={0}
+            value={reportDate}
+            //@ts-ignore
+            mode={"date"}
+            display="default"
+            onChange={onChange}
+          />
+        )}
+        {isTimePickerVisible && (
+          <DateTimePicker
+            testID="dateTimePicker"
+            timeZoneOffsetInMinutes={0}
+            value={reportTime}
+            //@ts-ignore
+            mode={"time"}
+            is24Hour={true}
+            display="default"
+            onChange={onChange}
+          />
+        )}
+        <View style={styles.upload}>
+        </View>
+      </View>
+      <PhotoUpload photoPickerTitle={"Select photo"}>
+            <Upload />
+          <Picture />
+      </PhotoUpload>
+    </>
   );
 });
